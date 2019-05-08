0b1f1370a3f59a33446be5434d7172f2dd62f76e
Nicolas Barbotte <nicolas@barbotte.net>
2019-05-08 09:11:48 +0200

    update dependencies

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,11 +1,11 @@
 var gulp = require('gulp'),
-browserify = require('browserify'),
-del = require('del'),
-reactify = require('reactify'),
-source = require('vinyl-source-stream'),
-stylus = require('gulp-stylus'),
-uglify = require('gulp-uglify'),
-cssmin = require('gulp-cssmin');
+  browserify = require('browserify'),
+  del = require('del'),
+  reactify = require('reactify'),
+  source = require('vinyl-source-stream'),
+  stylus = require('gulp-stylus'),
+  uglify = require('gulp-uglify'),
+  cssmin = require('gulp-cssmin');
 
 var paths = {
   css: ['./styles/**/*.styl'],
@@ -13,43 +13,44 @@ var paths = {
   js: ['./scripts/**/*.js'],
 };
 
-gulp.task('clean', function(done) {
-  del(['build'], done);
+gulp.task('clean', async done => {
+  await del(['build']);
+  done();
 });
 
-gulp.task('css', ['clean'], function() {
+gulp.task('css', gulp.series('clean', function () {
   return gulp.src(paths.css)
-  .pipe(stylus({ 'include css': true }))
-  .pipe(gulp.dest('styles'));
-});
+    .pipe(stylus({ 'include css': true }))
+    .pipe(gulp.dest('styles'));
+}));
 
-gulp.task('cssmin', ['css'], function() {
+gulp.task('cssmin', gulp.series('css', function () {
   return gulp.src('styles/main.css')
-  .pipe(cssmin())
-  .pipe(gulp.dest('styles'));
-});
+    .pipe(cssmin())
+    .pipe(gulp.dest('styles'));
+}));
 
-gulp.task('js', ['clean'], function() {
+gulp.task('js', gulp.series('clean', function () {
   return browserify({
     entries: [paths.r_js],
     transform: [reactify],
     extensions: '.jsx'
   })
-  .bundle()
-  .pipe(source('scripts/bundle.js'))
-  .pipe(gulp.dest('.'));
-});
+    .bundle()
+    .pipe(source('scripts/bundle.js'))
+    .pipe(gulp.dest('.'));
+}));
 
-gulp.task('uglify', ['js'], function(){
+gulp.task('uglify', gulp.series('js', function () {
   return gulp.src('scripts/bundle.js')
-  .pipe(uglify())
-  .pipe(gulp.dest('scripts'));
-});
+    .pipe(uglify())
+    .pipe(gulp.dest('scripts'));
+}));
 
-gulp.task('watch', function() {
+gulp.task('watch', function () {
   gulp.watch(paths.css, ['css']);
   gulp.watch(paths.r_js, ['js']);
 });
 
-gulp.task('default', ['css', 'js']);
-gulp.task('build', ['cssmin', 'uglify']);
+gulp.task('default', gulp.parallel('css', 'js'));
+gulp.task('build', gulp.parallel('cssmin', 'uglify'));
